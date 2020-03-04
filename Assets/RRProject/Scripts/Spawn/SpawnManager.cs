d6c631dd43639ac112e7100a83b779c08ca29974
Joseph Ballantyne <josephballantyne@outlook.com>
2020-03-04 18:37:48 -0500

    // added groupspawn class for managing a group of spawns // added the spawn class // added and started the spawn manager class

--- Assets/RRProject/Scripts/Spawn/SpawnManager.cs
+++ Assets/RRProject/Scripts/Spawn/SpawnManager.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// spawn manager communicates and coordinates spawns accross the spawngroups
 public class SpawnManager : MonoBehaviour
 {
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,30 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if this is beyond the 10th wave use the infinite scaling waves
+        if()
+        {
+
+        }
     }
+
+    // spawns a new wave
+    void spawnWave()
+    {
+
+    }
+
+    // structures the new wave
+    void structureWave()
+    {
+
+    }
+
+    // rolls the next wave to run 
+    void rollWave()
+    {
+
+    }
+
+    
 }
--- Assets/RRProject/Scripts/Spawn/Spawner.cs
+++ Assets/RRProject/Scripts/Spawn/Spawner.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
     // variables for storing all enemy spawn types
     public GameObject normal;
     public GameObject kamikaze;
@@ -12,31 +11,46 @@ public class Spawner : MonoBehaviour
     public GameObject cerberus;
     public GameObject mirror;
 
-
     // tracks whether or not this spawn can spawn tank enemies
-    bool mSpawn;
+    public bool mSpawn;
 
     // tracks whether or not this spawn is occupied
     public bool open;
 
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     // spawns a single enemy of the passed type
-    void spawnEnemy(string type)
+    public void spawnEnemy(string type)
     {
-
+        // spawn the proper enemy depending on the passed enemy type
+        switch (type)
+        {
+            case "tank":
+                Instantiate(tank, transform.position, transform.rotation);
+                break;
+            case "normal":
+                Instantiate(normal, transform.position, transform.rotation);
+                break;
+            case "kamikaze":
+                Instantiate(kamikaze, transform.position, transform.rotation);
+                break;
+            case "cerberus":
+                Instantiate(cerberus, transform.position, transform.rotation);
+                break;
+            case "mirror":
+                Instantiate(cerberus, transform.position, transform.rotation);
+                break;
+        }
+        // set this spawn as occupied
+        open = false;
     }
 
     // tracks whether or not this spawn is occupied
