91d4216267767de60b3811cbf79efe3cf14fc52f
Quinn <45831396+ReddyTheYeddy@users.noreply.github.com>
2020-03-05 18:02:01 -0500

    Added New End Wall Art

--- Assets/RRProject/Scripts/EndWall.cs
+++ Assets/RRProject/Scripts/EndWall.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class EndWall : MonoBehaviour
 {
+    public Sprite wall1;
+    public Sprite wall2;
+    public Sprite wall3;
+    public Sprite wall4;
+    public Sprite wall5;
+
     public float wallHealth;
     public LoadScene lScene;
 
@@ -22,13 +28,18 @@ public class EndWall : MonoBehaviour
 
     void CheckWallState()
     {
-        if (wallHealth > 0)
-        {
-            GetComponent<SpriteRenderer>().color = new Color(0, wallHealth / 100, 1);
-        }
+        if (wallHealth > 80)
+            GetComponent<SpriteRenderer>().sprite = wall1; //.color = new Color(0, wallHealth / 100, 1);
+        else if (wallHealth > 60)
+            GetComponent<SpriteRenderer>().sprite = wall2;
+        else if (wallHealth > 40)
+            GetComponent<SpriteRenderer>().sprite = wall3;
+        else if (wallHealth > 20)
+            GetComponent<SpriteRenderer>().sprite = wall4;
         else if (wallHealth <= 0)
         {
-            GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
+            GetComponent<SpriteRenderer>().sprite = wall5;
+            //GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
             lScene.LoadNewScene("Score");
         }
     }
