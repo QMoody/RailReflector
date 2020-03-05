9d8d303b649a3ac3ab09938eca55066470315ec4
Joseph Ballantyne <josephballantyne@outlook.com>
2020-03-04 19:17:41 -0500

    // removed wave // made wave a serialized class of spawnmanager

--- Assets/RRProject/Scripts/Spawn/SpawnManager.cs
+++ Assets/RRProject/Scripts/Spawn/SpawnManager.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 // spawn manager communicates and coordinates spawns accross the spawngroups
 public class SpawnManager : MonoBehaviour
 {
+    // stores all premade waves
+    public List<Wave> waves;
+    int pWCount;
 
-
-
-
+    // stores the number of the wave we are on
+    int wave;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // number of waves
+        pWCount += waves.Count;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if this is beyond the 10th wave use the infinite scaling waves
-        if()
+        // if this is beyond the final premade wave, construct the wave using infinite scaling
+        if((waves.Count - 1) < wave)
         {
-
+            structureWave();
         }
     }
 
@@ -32,17 +35,31 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    // structures the new wave
+    // structures the next wave
     void structureWave()
     {
 
     }
 
-    // rolls the next wave to run 
+    // rolls the unit type for the next wave
     void rollWave()
     {
 
     }
 
-    
+    // !! WAVES !!
+    // Wave 1
+    [System.Serializable] 
+    public class Wave
+    {   
+        // store the number of enemies in this wave
+        public int normal;
+        public int kamikaze;
+        public int tank;
+        public int cerberus;
+        public int mirror;
+        // scalar that multiplies the enemies health
+        public float healthScalar;
+    }
+
 }
