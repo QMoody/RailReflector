2915bab1990c9c5fafdd7eddb3549c53417de5d0
David de la Peña <david.dlpf@gamil.com>
2020-03-05 21:45:53 -0400

    background

---
+++ Assets/RRProject/Scripts/BackgroundParalax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParalax : MonoBehaviour
+{
+    public float speed;
+
+    public float resetPos;
+    public float resetToPos;
+    
+    
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if(transform.position.y <= resetPos)
+        {
+            transform.position = new Vector3(transform.position.x, resetToPos, transform.position.z);
+        }
+    }
+}
