14c009fc465f7fec9ecc816d6079a0c018298dfe
David de la Peña <david.dlpf@gamil.com>
2020-03-05 20:17:07 -0400

    Insert coin mechanic

---
+++ Assets/RRProject/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public LoadScene loadScene;
+    
+    void Start()
+    {
+        PlayerPrefs.SetInt("Player1", 0);
+        PlayerPrefs.SetInt("Player2", 0);
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlayerPrefs.SetInt("Player1", 1);
+            loadScene.LoadNewScene("Tutorial");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayerPrefs.SetInt("Player2", 1);
+            loadScene.LoadNewScene("Tutorial");
+        }
+    }
+}
--- Assets/RRProject/Scripts/Systems/LevelManager.cs
+++ Assets/RRProject/Scripts/Systems/LevelManager.cs
@@ -12,15 +12,55 @@ public class LevelManager : Singletone<LevelManager>
     public int player1Score;
     public int player2Score;
 
+    public GameObject Player1;
+    public GameObject Player2;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        player1Active = PlayerPrefs.GetInt("Player1") == 0 ? false : true;
+        player2Active = PlayerPrefs.GetInt("Player2") == 0 ? false : true;
+
+        if(!player1Active && !player2Active)
+        {
+            player1Active = true;
+        }
+        CreatePlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreatePlayer()
     {
-        
+        if (player1Active)
+        {
+            Instantiate(Player1, new Vector3(-10, -10, 0), Quaternion.identity);
+        }
+
+        if (player2Active)
+        {
+            Instantiate(Player2, new Vector3(10, -10, 0), Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if(player1Active)
+            {
+                return;
+            }
+            Instantiate(Player1, new Vector3(-10, -10, 0), Quaternion.identity);
+            player1Active = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (player2Active)
+            {
+                return;
+            }
+            Instantiate(Player2, new Vector3(10, -10, 0), Quaternion.identity);
+            player2Active = true;
+        }
     }
 }
